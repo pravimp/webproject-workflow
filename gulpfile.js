99a81148aca78d200ea2fcb08578ef1458325e06
pravi <praveenmp01@gmail.com>
2018-11-06 14:57:08 +0530

    sass compiler compass added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass');
 
 var coffeeSources = ['components/coffee/tagline.coffee'],
 	jsSources = [
@@ -9,7 +11,8 @@ var coffeeSources = ['components/coffee/tagline.coffee'],
 		'components/scripts/rclick.js',
 		'components/scripts/tagline.js',
 		'components/scripts/template.js'
-	];
+	],
+	sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee',function(){
 	gulp.src(coffeeSources)
@@ -21,6 +24,18 @@ gulp.task('coffee',function(){
 gulp.task('js', function(){
 	gulp.src(jsSources)
 	.pipe(concat('script.js'))
+	.pipe(browserify())
 	.pipe(gulp.dest('builds/development/js'))
 });
+
+gulp.task('compass', function(){
+	gulp.src(sassSources)
+	.pipe(compass({
+		sass: 'components/sass',
+		image: 'builds/development/images',
+		style: 'expanded'
+	})
+	.on('error', gutil.log))
+	.pipe(gulp.dest('builds/development/css'))
+})
 
